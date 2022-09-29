c87b5ee3928f92f823423fa6be73dbaddda5d44f
ClintonTSR <70693551+ClintonTSR@users.noreply.github.com>
2022-09-29 09:53:40 +0800

    add header and landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button'
+import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material/material.module';
+
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent },
+]
+
 @NgModule({
   declarations: [
     AppComponent,
+    LandingPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -16,6 +28,8 @@ import { MatButtonModule } from '@angular/material/button'
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatButtonModule,
+    MaterialModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
